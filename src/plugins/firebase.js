08c5f34b887bee32822d34600d80a8b75554c68c
塚原彩 <ayatsukahara0112@gmail.com>
2020-08-08 17:47:50 +0900

    プロジェクトの設定

---
+++ src/plugins/firebase.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueFire)
+
+const firebaseApp = firebase.initializeApp({
+  // ここにfirebaseのapi情報をコピペします。
+  apiKey: "AIzaSyBE_Rm6QanzA-VZTu8qCWM1bQUMLMnp6ow",
+  authDomain: "vue-bord-39ce4.firebaseapp.com",
+  databaseURL: "https://vue-bord-39ce4.firebaseio.com",
+  projectId: "vue-bord-39ce4",
+  storageBucket: "vue-bord-39ce4.appspot.com",
+  messagingSenderId: "442754989670",
+  appId: "1:442754989670:web:f84d0fdf5c3a72d61ab7a7",
+  measurementId: "G-VV6KM1K06G"
+
+});
+
+export const db = firebaseApp.firestore();
