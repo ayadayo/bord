4028e4adcefef304f255027de8aea6cd37d0b461
塚原彩 <ayatsukahara0112@gmail.com>
2020-08-08 20:13:32 +0900

    コピペで動くことが確認できた

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <v-app>
     <v-container>
       <ChatBoard/>
+      <ChatFrom/>
     </v-container>
   </v-app>
 </template>
 
 <script>
   import ChatBoard from './components/ChatBoard'
+  import ChatFrom from './components/Form'
 
   export default {
     name: 'App',
     components: {
-      ChatBoard
+      ChatBoard,
+      ChatFrom
     },
   }
 </script>
